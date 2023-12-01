d85a49f2645f32b5f53c9db8fc7b0ce97f3ca5ed
Leo-Turible <turibleleo@gmail.com>
2023-12-01 18:00:13 +0100

    buttons actived/disabled by conditions

--- index.js
+++ index.js
@@ -19,9 +19,10 @@ async function connectToOBS() {
         const { currentProgramSceneName } = await obs.call('GetCurrentProgramScene');
         console.log('Current program scene:', currentProgramSceneName);
 
-
-
         enableButtons();
+
+        // Mettre à jour l'état des boutons de changement de scène
+        disableSceneButtons(currentProgramSceneName);
     } catch (error) {
         console.error('Failed to connect', error.code, error.message);
     }
@@ -34,37 +35,31 @@ async function disconnectFromOBS() {
 }
 
 async function startStreaming() {
-    // Start streaming with the 'recording' and 'high_quality' settings
-    // await obs.call('StartStreaming', {
-    //     'stream': {
-    //         'type': 'rtmp_custom',
-    //         'settings': {
-    //             'server': 'rtmp://localhost:1935/live',
-    //             'key': 'stream',
-    //         },
-    //     },
-    //     'recording': true,
-    //     'high_quality': true,
-    // });
     try {
         await obs.call('StartStream');
         console.log('Streaming started');
+
+        // Mettre à jour l'état des boutons
+        document.getElementById('startStreamBtn').disabled = true;
+        document.getElementById('stopStreamBtn').disabled = false;
     } catch (error) {
         console.error('Failed to start streaming', error.code, error.message);
     }
-    // Execute the GetStreamStatus request
-    const { GetStreamStatus } = await obs.call('GetStreamStatus');
-    console.log('Streaming started', GetStreamStatus);
 }
 
 async function stopStreaming() {
     try {
         await obs.call('StopStream');
         console.log('Streaming stopped');
+
+        // Mettre à jour l'état des boutons
+        document.getElementById('startStreamBtn').disabled = false;
+        document.getElementById('stopStreamBtn').disabled = true;
     } catch (error) {
         console.error('Failed to stop streaming', error.code, error.message);
     }
 }
+
 
 async function changeToTestScene() {
     try {
@@ -75,6 +70,9 @@ async function changeToTestScene() {
         // Execute the GetCurrentProgramScene request
         const { currentProgramSceneName } = await obs.call('GetCurrentProgramScene');
         console.log('Current program scene:', currentProgramSceneName);
+
+        // Mettre à jour l'état des boutons de changement de scène
+        disableSceneButtons(currentProgramSceneName);
     } catch (error) {
         console.error('Failed to change scene', error.code, error.message);
     }
@@ -90,9 +88,24 @@ async function changeToScene() {
         // Execute the GetCurrentProgramScene request
         const { currentProgramSceneName } = await obs.call('GetCurrentProgramScene');
         console.log('Current program scene:', currentProgramSceneName);
+
+        // Mettre à jour l'état des boutons de changement de scène
+        disableSceneButtons(currentProgramSceneName);
     } catch (error) {
         console.error('Failed to change scene', error.code, error.message);
     }
+}
+
+// Fonction pour désactiver les boutons de changement de scène si on est déjà sur la même scène
+function disableSceneButtons(currentSceneName) {
+    const testSceneBtn = document.getElementById('changeSceneBtn');
+    const sceneBtn = document.getElementById('changeToSceneBtn');
+
+    // Désactiver le bouton "Change to Scene 'Test'" si on est déjà sur la scène 'Test'
+    testSceneBtn.disabled = currentSceneName === 'Test';
+
+    // Désactiver le bouton "Change to Scene 'Scène'" si on est déjà sur la scène 'Scène'
+    sceneBtn.disabled = currentSceneName === 'Scène';
 }
 
 async function toggleMicrophoneMute() {
@@ -108,7 +121,7 @@ function enableButtons() {
     document.getElementById('connectBtn').disabled = true;
     document.getElementById('disconnectBtn').disabled = false;
     document.getElementById('startStreamBtn').disabled = false;
-    document.getElementById('stopStreamBtn').disabled = false;
+    // document.getElementById('stopStreamBtn').disabled = false;
     document.getElementById('changeSceneBtn').disabled = false;
     document.getElementById('changeToSceneBtn').disabled = false;
     document.getElementById('toggleMuteBtn').disabled = false;
